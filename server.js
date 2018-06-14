fdbdff6ddb5a0639338e5127158570075952db2c
David Guillen <david.guillen@onestop.com>
2018-06-14 01:08:51 -0700

    mongo and express

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express'); // Call the Express libs
 const mongoose =  require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const post = require('./routes/api/post');
 
 const app = express(); // Use a express
-
+// Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 // DB CONF
 const db = require('./config/keys').mongoURI;
